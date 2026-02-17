12fbaaa481bb4104b48dd1580e37855f9d8444f2
Samreen Jahan <jahansamreen370@gmail.com>
2026-02-17 23:42:26 +0530

    initial commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber:{
+        type: Number,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        enum: ['student','recruiter'],
+        required: true
+    },
+    profile:{
+        bio:{type:String},
+        skills:[{type: String}],
+        resume:{type: String},  //url to resume
+        resumeOriginalName:{type:String},
+        company:{type:mongoose.Schema.Types.ObjectId, ref:'Company'},//generating relation b/w comany and user table
+        profilePhoto:{
+            type:String,
+            default: ""
+        }
+    }
+},{timestamp: true});
+export const User=mongoose.model('User',userSchema);
